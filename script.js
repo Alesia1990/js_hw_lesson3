ac9bcdc7388a4cc21678acb818c646908b2fec86
Alesia <alesechkabest@gmail.com>
2022-05-28 05:30:36 +0300

    added task 3

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 //task 1
 
 let i = 1;
+
 while(i <= 50){
     console.log(i);
     i++;
-}
+};
 
 let a = 35;
+
 while(a >= 8){
     console.log(a);
     a--;
-}
+};
 
 //task 2
+
 let b = 89;
+
 while(b >= 11){
     document.write(b + "<br/>");
     b--;
+};
+
+//task 3
+
+let sum = 0;
+
+for(let i = 0; i <= 100; i++){
+    sum += i;
 }
+
+console.log(`сумма чисел 100 = ${sum}`);
+
+
