925159196fabed6f650116aa01f071e83a510075
Alesia <alesechkabest@gmail.com>
2022-05-28 05:41:19 +0300

    added task 5

--- script.js
+++ script.js
@@ -44,3 +44,11 @@ for(let i = 1; i <= 5; i++){
     }
     console.log(`Сумма чисел числа ${i} = ${sum2}`);
 }
+
+// task 5
+
+for(let i = 8; i <= 56; i++){
+    if(i % 2 == 1 ) continue;
+    console.log(i);
+}
+
